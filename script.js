b9c3ef2bffa0fdec46e26701a57fdca0dd3e7281
Yuxin <kayliefang1914@gmail.com>
2025-03-13 20:27:25 -0400

    ok

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleSection(id) {
-    let content = document.getElementById(id);
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-        content.style.animation = "fadeIn 0.5s ease-in-out";
-    } else {
-        content.style.display = "none";
-    }
-}
